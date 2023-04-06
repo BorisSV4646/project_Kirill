f4111ad833756e65edd43c9fba49afa571daed2a
Boris Vinnik <vinniklawyer@yandex.ru>
2023-04-06 18:10:42 +0800

    Change config hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomiclabs/hardhat-solhint");
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
-    hardhat: {},
+    hardhat: {
+      forking: {
+        url: process.env.INFURA_URL_ETH,
+        blockNumber: 16974700,
+      },
+    },
     goerly: {
       url: process.env.INFURA_URL_GOERLY,
       accounts: [process.env.PRIVATE_KEY],
@@ -23,9 +29,4 @@ module.exports = {
       },
     },
   },
-  // networks: {
-  //   hardhat: {
-  //     chainId: 1337,
-  //   },
-  // },
 };
